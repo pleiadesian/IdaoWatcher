79674060f5b32c58767b825236c6e189ef1ad306
pleiadesian <wzl574402791@outlook.com>
2020-01-11 16:07:28 +0800

    complete display message

--- IdaoWatcher/watch_limit.py
+++ IdaoWatcher/watch_limit.py
@@ -6,35 +6,19 @@
 """
 import tushare as ts
 import time
+import sys
 import watch_limit_main
 from tkinter import messagebox
 from tkinter import ttk
 import tkinter as tk
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsPixmapItem, QFileDialog
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import QTimer
 
 CODE = '600075'
 CODE1 = '300541'
-INTERVAL = 1
+INTERVAL = 5000
 THRESHOLD = 0.95
-DISPLAY = 3
-
-
-# def _async_raise(tid, exctype):
-#     """raises the exception, performs cleanup if needed"""
-#     tid = ctypes.c_long(tid)
-#     if not inspect.isclass(exctype):
-#         exctype = type(exctype)
-#     res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
-#     if res == 0:
-#         raise ValueError("invalid thread id")
-#     elif res != 1:
-#         # """if it returns a number greater than one, you're in trouble,
-#         # and you should call it again with exc=NULL to revert the effect"""
-#         ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
-#         raise SystemError("PyThreadState_SetAsyncExc failed")
-#
-#
-# def stop_thread(thread):
-#     _async_raise(thread.ident, SystemExit)
 
 
 def get_new_a1p():
@@ -45,25 +29,19 @@ def get_new_b1v():
     return int(ts.get_realtime_quotes(CODE)['b1_v'].values[0])
 
 
-def alert(code):
-    messagebox.showinfo("提示", code + " 出现开板迹象")
-#
-#
-# def auto_alert(code):
-#     t1 = threading.Thread(target=alert, args=(CODE,))
-#     t1.start()
-#     time.sleep(DISPLAY)
-#     stop_thread(t1)
+class PictureView(QMainWindow, watch_limit_main.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(PictureView, self).__init__(parent)
+        self.setupUi(self)
 
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.main_loop)
 
-# assume following logic happens every INTERVAL seconds
-i = 0
-root = tk.Tk()
-root.withdraw()
-if __name__ == '__main__':
-    b1_v_prev = None
-    while True:
-        time.sleep(INTERVAL)
+        self.i = 0
+        self.b1_v_prev = None
+        self.timer.start(INTERVAL)
+
+    def main_loop(self):
         a1_p = get_new_a1p()
         b1_v = get_new_b1v()
 
@@ -71,10 +49,19 @@ if __name__ == '__main__':
         if a1_p == 0:
             # limit keeped, watch its volume
             print("封停")
-            if a1_p is None:
-                b1_v_prev = b1_v
+            if self.b1_v_prev is None:
+                self.b1_v_prev = b1_v
             else:
-                # if b1_v / b1_v_prev < THRESHOLD:
-                print(1)
+                if b1_v / self.b1_v_prev < THRESHOLD:
+                    self.label.setText(CODE + " 出现开板迹象")
+                    self.b1_v_prev = b1_v
         else:
             print("未封停")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    picView = PictureView()
+    picView.show()
+    app.exec_()
+
--- IdaoWatcher/watch_limit_main.py
+++ IdaoWatcher/watch_limit_main.py
@@ -13,15 +13,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(471, 308)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(220, 230, 341, 71))
+        self.label.setGeometry(QtCore.QRect(60, 10, 321, 201))
         self.label.setObjectName("label")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 471, 22))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
