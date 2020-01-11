9af46bafd5320ccbdc46e8d59e078a19fca727d7
pleiadesian <wzl574402791@outlook.com>
2020-01-11 14:58:40 +0800

    add message

--- IdaoWatcher/watch_limit.py
+++ IdaoWatcher/watch_limit.py
@@ -5,21 +5,39 @@
 监听特定股票的封停是否有打开的迹象
 """
 import tushare as ts
+import time
+from tkinter import messagebox
 
 CODE = '600075'
 CODE1 = '300541'
+INTERVAL = 1
+THRESHOLD = 0.95
 
 
 def get_new_a1p():
-    return float(ts.get_realtime_quotes(CODE)['a1_p'].values[0])  # Single stock symbol
+    return float(ts.get_realtime_quotes(CODE)['a1_p'].values[0])
 
 
-a1_p = get_new_a1p()
-a1_v_prev = None
+def get_new_b1v():
+    return int(ts.get_realtime_quotes(CODE)['b1_v'].values[0])
 
-# use a1_p == 0.00 to judge if limit has been broken
-if a1_p == 0:
-    # limit keeped, watch its volume
-    print("封停")
-else:
-    print("未封停")
+
+# assume following logic happens every INTERVAL seconds
+if __name__ == '__main__':
+    b1_v_prev = None
+    while True:
+        time.sleep(INTERVAL)
+        a1_p = get_new_a1p()
+        b1_v = get_new_b1v()
+
+        # use a1_p == 0.00 to judge if limit has been broken
+        if a1_p == 0:
+            # limit keeped, watch its volume
+            print("封停")
+            if a1_p is None:
+                b1_v_prev = b1_v
+            else:
+                # if b1_v / b1_v_prev < THRESHOLD:
+                messagebox.showinfo("提示", CODE + " 出现开板迹象")
+        else:
+            print("未封停")
