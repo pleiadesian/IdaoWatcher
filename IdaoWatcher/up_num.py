102133d1b4d6956dfa4023f181104bfe300ad616
wzl <wzl574402791@outlook.com>
2020-01-18 11:43:50 +0800

    Update up_num.py

--- IdaoWatcher/up_num.py
+++ IdaoWatcher/up_num.py
@@ -12,7 +12,7 @@ def calc_60_up_num(code):
     now_time=datetime.datetime.now().strftime('%Y-%m-%d') #今天的日期（年-月-日）
     past_time=(datetime.datetime.now()-datetime.timedelta(days=3)).strftime('%Y-%m-%d') #认为不可能连着12根阳线，只往前推3周
 
-    a=ts.get_hist_data(stock_ticker,start=past_time,end=now_time,ktype='60')
+    a=ts.get_hist_data(code,start=past_time,end=now_time,ktype='60')
     #######print(a)
     for i in range(11):
         if(a.iat[i,2]>a.iat[i,0]+0.01)and(a.iat[i+1,2]>a.iat[i+1,0]+0.01):
