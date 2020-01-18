ee5e6536ed1d4ba38c5a93ef7cb726b0c882ac2a
pleiadesian <wzl574402791@outlook.com>
2020-01-18 08:48:38 +0800

    complete sina spider

--- IdaoWatcher/api/storage.py
+++ IdaoWatcher/api/storage.py
@@ -10,7 +10,11 @@ import pandas as pd
 from random import randint
 from multiprocessing.pool import ThreadPool
 from urllib.request import urlopen, Request
+import requests
+import urllib
 import api.ts_map
+
+DEBUG = 0
 
 DATA_COLS = ['name', 'open', 'pre_close', 'price', 'high', 'low', 'bid', 'ask', 'volume', 'amount', 'b1_v', 'b1_p',
              'b2_v', 'b2_p', 'b3_v', 'b3_p', 'b4_v', 'b4_p', 'b5_v', 'b5_p', 'a1_v', 'a1_p', 'a2_v', 'a2_p', 'a3_v',
@@ -30,10 +34,25 @@ def process_plaintext(index, reg, reg_sym):
     :param reg_sym: regular expression for symbols
     :return: data frame for a bunch of stock
     """
+    # TODO: add index on code column
     request = Request('%shq.%s/rn=%s&list=%s' % ('http://', 'sinajs.cn',
                                                  random(), api.ts_map.code_list[index]))
-    text = urlopen(request, timeout=10).read()
-    text = text.decode('GBK')
+    resp = None
+    not_get = True
+    while not_get:
+        try:
+            # text = urlopen(request, timeout=1).read()
+            resp = requests.get('%shq.%s/rn=%s&list=%s' % ('http://', 'sinajs.cn',
+                                random(), api.ts_map.code_list[index]), timeout=3)
+            not_get = False
+            if resp is None:
+                not_get = True
+        except requests.exceptions.RequestException as e:
+            time.sleep(1)
+            not_get = True
+
+    text = resp.text
+    # text = text.decode('GBK')
     data = reg.findall(text)
     syms = reg_sym.findall(text)
     data_list = []
@@ -51,7 +70,6 @@ def process_plaintext(index, reg, reg_sym):
         df[txt] = df[txt].map(lambda x: x[:-2])
     return df
 
-
 class Storage:
     def __init__(self):
         self.realtime_quotes = None
@@ -63,14 +81,29 @@ class Storage:
         """
         scratch realtime data and store it locally
         """
-        # TODO: multi-threading
+        if DEBUG == 1:
+            start_time = time.time()
         args = []
-        for i in range(0, 8):
+        for i in range(0, 5):
             args.append((i, self.reg, self.reg_sym))
-
         p = ThreadPool()
         df_list = p.starmap(process_plaintext, args)
-        df_curr = pd.concat(df_list)
+
+        args_remain = []
+        for i in range(5, 8):
+            args_remain.append((i, self.reg, self.reg_sym))
+        df_list_remain = p.starmap(process_plaintext, args_remain)
+        df_curr = pd.concat(df_list + df_list_remain)
+        if DEBUG == 1:
+            end_time = time.time()
+            print(end_time - start_time)
+            for i in range(0, 5):
+                print(df_list[i][0])
+                print(df_list[i][1][:50])
+            for i in range(0, 3):
+                print(df_list_remain[i][0])
+                print(df_list_remain[i][1][:50])
+            print('\n')
         self.realtime_quotes = df_curr
 
     def get_realtime_storage(self):
@@ -95,5 +128,7 @@ class Storage:
 
 if __name__ == '__main__':
     storage = Storage()
-    storage.update_realtime_storage()
-    print(storage.get_realtime_storage())
+    while True:
+        time.sleep(2)
+        storage.update_realtime_storage()
+        print(storage.get_realtime_storage())
