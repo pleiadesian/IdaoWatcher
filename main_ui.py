7713e0366910b66a41ff71dd0847953c47ed0c56
pleiadesian <wzl574402791@outlook.com>
2020-02-02 13:19:58 +0800

    communication based on shared memory

--- main_ui.py
+++ main_ui.py
@@ -4,14 +4,27 @@
 @ Datetime: 2020-02-02 11:02
 """
 import sys
+import time
+import multiprocessing
+import main as backend
+from random import randint
+from multiprocessing import Value, Manager
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QWidget
-from frontend import main
+from frontend import main as frontend
 
 
-class MainUi(QMainWindow, main.Ui_Dialog):
+class MainUi(QMainWindow, frontend.Ui_Dialog):
     def __init__(self):
         super(MainUi, self).__init__()
         self.setupUi(self)
+
+
+def start_ui(codes):
+    # main_ui = MainUi()
+    # main_ui.show()
+    while True:
+        print(codes)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
