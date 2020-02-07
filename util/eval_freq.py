01f0fbef8c41e4ad0739ae87124dfb888c8b1111
Cei1ing <lincong_ceiling@126.com>
2020-02-07 17:34:08 +0800

    fix a bug in util/eval_freq get_attack_time_list

--- util/eval_freq.py
+++ util/eval_freq.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 def get_attack_time_list(stock_id,log_file_path):
     """
     
-    stock_id: an int indicating the id of a stock
+    stock_id: a string indicating the id of a stock
     log_file_path: a string path to the log file you want to read
     
     """
@@ -27,7 +27,7 @@ def get_attack_time_list(stock_id,log_file_path):
     
     return_list = []
     for line in time_share_attack_raw:
-        if str(stock_id) in line:
+        if stock_id in line:
             return_list.append(line[0]+' '+line[1])
             
     return return_list
@@ -75,15 +75,13 @@ def get_attack_distribution(log_file_path):
     
     plt.figure(figsize=(16,10))
     ticks = np.arange(len(pre_neckline_ctrs))
-    print(ticks)
     plt.bar(ticks, pre_neckline_ctrs, label = "attacks before neckline check")
     ticks = np.arange(len(post_neckline_ctrs))
-    print(ticks)
     plt.bar(ticks, post_neckline_ctrs, label = "attacks after neckline check")
     plt.legend(loc = 'best',fontsize = 15)
     plt.grid()
     plt.show()
 
 if __name__ == "__main__":
-    print(get_attack_time_list(600311,"20200204.log"))
-    get_attack_distribution("20200204.log")
+    print(get_attack_time_list('600311',"20200207.log"))
+    get_attack_distribution("20200207.log")
